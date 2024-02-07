94a58f1f64eeee888cac0336b779a767bdfdb3f3
halef.juvencio <halef.juvencio@consilux.com.br>
2024-02-07 16:56:45 -0300

    feat:primeiro commit

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
+}
+
+public class TodoItemDTO
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public bool IsComplete { get; set; }
+
+}
